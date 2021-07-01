1970f22afc4417ac02ae8a6506adc9a4219658a2
emert117 <emremert117@gmail.com>
2021-07-01 22:10:51 +0300

    Add project files.

---
+++ CarDealer.ConsoleUI/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CarDealer.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Car Dealer Software!");
+            Console.ReadLine();
+        }
+    }
+}
