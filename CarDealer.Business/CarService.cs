333a7f4102cb169b1394920fe4852023be01d1cc
emert117 <emremert117@gmail.com>
2021-07-03 12:08:41 +0300

    added comments

--- CarDealer.Business/CarService.cs
+++ CarDealer.Business/CarService.cs
@@ -10,10 +10,23 @@ namespace CarDealer.Business
 {
     public class CarService
     {
+        
         public List<Car> GetCars()
         {
+            // recheck user permissions
+            // CheckUserRole("Admin,PremiumUser")
+            // Cross Cutting Concerns
+
+            // apply restrictions
+
+            // request from cache
+
+            // get from DB
             CarDAL carDAL = new CarDAL();
             var cars = carDAL.GetCarsFromDB();
+            // apply rules that DB can't run
+
+            // finally return result
             return cars;
         }
     }
--- CarDealer.DataAccess/CarDAL.cs
+++ CarDealer.DataAccess/CarDAL.cs
@@ -9,16 +9,27 @@ namespace CarDealer.DataAccess
 {
     public class CarDAL
     {
-        // seed cars
+        
         List<Car> cars = new List<Car>()
             {
                 new Car() {ID = 1, Make = "Opel", Color = System.Drawing.Color.Blue, Description = "Aile arabası", Mileage = 12000, ModelYear = 2019, Price = 320000M, TransmissionType = Transmission.Automatic},
                 new Car() {ID = 2, Make = "Nissan", Color = System.Drawing.Color.Red, Description = "Az yakar çok kaçar", Mileage = 87900, ModelYear = 2015, Price = 28000M, TransmissionType = Transmission.Manual},
+                new Car() {ID = 3, Make = "Ford", Color = System.Drawing.Color.Red, Description = "Çıtır hasarlı", Mileage = 45600, ModelYear = 2017, Price = 31000M, TransmissionType = Transmission.Manual},
             };
 
         public List<Car> GetCarsFromDB()
         {
             // connect to DB, get all cars
+            return GetCarsFromMSSQLServer();
+        }
+
+        private List<Car> GetCarsFromMSSQLServer()
+        {
+            return cars;
+        }
+
+        private List<Car> GetCarsFromOracle()
+        {
             return cars;
         }
 
